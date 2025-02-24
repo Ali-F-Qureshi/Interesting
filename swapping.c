9dcd054f6c0abbeba43a33d87ca6a38ced1c77a2
Ali-F-Qureshi <aliqureshi20@outlook.com>
2025-02-24 18:49:57 -0500

    Create swapping.c
    Wrote two functions:
     - swap_two_var
     - swap_one_line

---
+++ swapping.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+swap two variables withoutthe use of a third variable
+*/
+void swap_two_var(int a, int b){
+  printf("a = %d\n
+          b = %d\n\n", 
+          a, b);
+  printf("a = a + b\n
+          b = a - b\n
+          a = a - b\n");
+
+  a = a + b;
+  b = a - b;
+  a = a - b;
+
+  printf("a = %d\n
+          b = %d\n\n", 
+          a, b);
+}
+
+/*
+swap two variables in one line
+*/
+void swap_one_line(int a, int b){
+  printf("a = %d\n
+          b = %d\n\n", 
+          a, b);
+  printf("a = b + a - (b = a)\n");
+
+  a = b + a - (b = a);
+
+  printf("a = %d\n
+          b = %d\n\n", 
+          a, b);
+}
