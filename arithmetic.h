2e6583b100f3c4d69b1fc57f195ea807b1d252d5
Ali-F-Qureshi <aliqureshi20@outlook.com>
2025-02-24 19:02:43 -0500

    Update arithmetic.h
    Added doc for swap_two_var and swap_one_line

--- arithmetic.h
+++ arithmetic.h
@@ -2,7 +2,13 @@
 #define ARITHMETIC_H
 
 //  swapping.c
+/*
+swap two variables withoutthe use of a third variable
+*/
 void swap_two_var(int a, int b);
+/*
+swap two variables in one line
+*/
 void swap_one_line(int a, int b);
 
 #endif
